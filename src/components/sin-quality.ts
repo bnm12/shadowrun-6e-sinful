497dbca6b2c7d903b273ed67c1458de214ff7686
Benjamin Kristensen <bnm12@users.noreply.github.com>
2025-07-09 11:05:26 +0200

    Update sin-quality.ts

--- src/components/sin-quality.ts
+++ src/components/sin-quality.ts
@@ -1,11 +1,11 @@
-export const SinQuality {
+export const SinQuality = {
   LEVEL_1: 1,
   LEVEL_2: 2,
   LEVEL_3: 3,
   LEVEL_4: 4,
   LEVEL_5: 5,
   LEVEL_6: 6,
-}
+};
 
 export const SinQualityFlairMap: Record<SinQuality, string> = {
   [SinQuality.LEVEL_1]: "1 - Barely Exists",
